827f328bbd4b5b3a107456d362c8d81057f85afa
keeganpa <keeganpa76@hotmail.fr>
2018-05-15 15:24:05 +0200

    round rectif

--- DES/src/core/RoundFonctionSteps.java
+++ DES/src/core/RoundFonctionSteps.java
@@ -54,7 +54,8 @@ public class RoundFonctionSteps {
 	
 	private int[][][] SBOX = {SBOX1, SBOX2, SBOX3, SBOX4, SBOX5, SBOX6, SBOX7, SBOX8};
 	    
-	private int [][] P={{16 ,7 ,20 ,21},
+	private int [][] P={
+			{16 ,7 ,20 ,21},
 			{29, 12, 28, 17},
 		    {1, 15, 23, 26},
 		    {5, 18, 31, 10},
@@ -63,6 +64,29 @@ public class RoundFonctionSteps {
 		    {19, 13, 30, 6},
 		    {22, 11, 4, 25}
 		    };
+	
+	private int[][] IP ={ 
+			{58, 50, 42, 34, 26, 18, 10, 2},
+			{60, 52, 44, 36, 28, 20, 12, 4},
+			{62, 54, 46, 38, 30, 22, 14, 6},
+			{64, 56, 48, 40, 32, 24, 16, 8},
+			{57, 49, 41, 33, 25, 17, 9,  1},
+			{59, 51, 43, 35, 27, 19, 11, 3},
+			{61, 53, 45, 37, 29, 21, 13, 5},
+			{63, 55, 47, 39, 31, 23, 15, 7}
+		};
+	
+	private int[][] IP1={
+			{40, 8, 48, 16, 56, 24, 64, 32},
+			{39, 7, 47, 15, 55, 23, 63, 31},
+			{38, 6, 46, 14, 54, 22, 62, 30},
+			{37, 5, 45, 13, 53, 21, 61, 29},
+			{36, 4, 44, 12, 52, 20, 60, 28},
+			{35, 3, 43, 11, 51, 19, 59, 27},
+			{34, 2, 42, 10, 50, 18, 58, 26},
+			{33, 1, 41, 9, 49, 17, 57, 25}
+		};
+	
 	
 	//ETable step
 	public Boolean[][] ETable(Boolean[][] input) {
@@ -163,14 +187,33 @@ public class RoundFonctionSteps {
 	}
 	
 	//Permutation step
-	public Boolean[][] P(Boolean[][] input) {
+	public Boolean[][] P(Boolean[][] input, int permutation) {
 		int inputRows = input.length;
 		int inputCols = input[0].length;
+		System.out.println(input.length);
+		System.out.println(input[0].length);
 		Boolean[][] output = new Boolean[inputRows][inputCols];
 		for (int i = 0; i < inputRows; i++) {
 			for (int j = 0; j < inputCols; j++) {
-				int num = P[i][j] - 1;
-				output[i][j] = input[((num - num % 4) / 4)%input.length][(num % 4)%input[0].length];
+				int num;
+				switch (permutation) {
+					case 1:
+						num = P[i][j] - 1;
+						break;
+					case 2:
+						num = IP[i][j] - 1;
+						break;
+					case 3:
+						num = IP1[i][j] - 1;
+						break;
+					default:
+						num = 0;
+						break;
+				}
+				
+				System.out.println((num - num % inputCols) / inputCols);
+				System.out.println(num % inputCols);
+				output[i][j] = input[(num - num % inputCols) / inputCols][num % inputCols];
 			}
 		}
 		return output;
--- DES/src/roundFonctions/DES3.java
+++ DES/src/roundFonctions/DES3.java
@@ -11,13 +11,43 @@ public class DES3 {
 	//DES roundFonction with Permutation missing and S-box replaced by E-1-table step
 	public Boolean[][] DES(Boolean[][] input, Boolean[] key) {
 		System.out.println("DES3");
-		// E-Table step
-		Boolean[][] output = roundFonctionSteps.ETable(input);
-		// XOR step
-		output = roundFonctionSteps.XOR(output, key);
-		// E-1-table step
-		output = roundFonctionSteps.E1Table(output);
+		input = roundFonctionSteps.P(input, 2);
+				
+		//separation in 2 blocks
+		Boolean[][] block1 = java.util.Arrays.copyOfRange(input, 0, 2);
+		Boolean[][] block2 = java.util.Arrays.copyOfRange(input, 2, 4);
 		
+		for (int i = 0; i < 16; i++) {
+			block1 = block2;
+			// E-Table step
+			Boolean[][] output = roundFonctionSteps.ETable(input);
+			// XOR step
+			output = roundFonctionSteps.XOR(output, key);
+			// E-1-table step
+			output = roundFonctionSteps.E1Table(output);
+			
+			block2 = roundFonctionSteps.XOR(block1, output);
+		}
+		
+		Boolean[][] output = new Boolean[16][4];
+		output[0] = block1[0];
+		output[1] = block1[1];
+		output[2] = block1[2];
+		output[3] = block1[3];
+		output[4] = block1[4];
+		output[5] = block1[5];
+		output[6] = block1[6];
+		output[7] = block1[7];
+		output[8] = block2[0];
+		output[9] = block2[1];
+		output[10] = block2[2];
+		output[11] = block2[3];
+		output[12] = block2[4];
+		output[13] = block2[5];
+		output[14] = block2[6];
+		output[15] = block2[7];
+		output = roundFonctionSteps.P(output, 3);
+
 		return output;
 	}
 }
