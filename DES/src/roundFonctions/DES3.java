1fa836355ac9019382460802e60f2fb9291c700f
humesy <39025189+humesy@users.noreply.github.com>
2018-05-23 03:49:31 +1000

    Update DES3.java

--- DES/src/roundFonctions/DES3.java
+++ DES/src/roundFonctions/DES3.java
@@ -45,22 +45,22 @@ public class DES3 {
 		
 		//get the blocks together again
 		Boolean[][] output = new Boolean[16][4];
-		output[0] = block1[0];
-		output[1] = block1[1];
-		output[2] = block1[2];
-		output[3] = block1[3];
-		output[4] = block1[4];
-		output[5] = block1[5];
-		output[6] = block1[6];
-		output[7] = block1[7];
-		output[8] = block2[0];
-		output[9] = block2[1];
-		output[10] = block2[2];
-		output[11] = block2[3];
-		output[12] = block2[4];
-		output[13] = block2[5];
-		output[14] = block2[6];
-		output[15] = block2[7];
+		output[0] = block2[0];
+		output[1] = block2[1];
+		output[2] = block2[2];
+		output[3] = block2[3];
+		output[4] = block2[4];
+		output[5] = block2[5];
+		output[6] = block2[6];
+		output[7] = block2[7];
+		output[8] = block1[0];
+		output[9] = block1[1];
+		output[10] = block1[2];
+		output[11] = block1[3];
+		output[12] = block1[4];
+		output[13] = block1[5];
+		output[14] = block1[6];
+		output[15] = block1[7];
 		
 		//final inverse permutation
 		output = roundFonctionSteps.P(output, 3);
